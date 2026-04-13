bfc9d98950adf655e161a1ecca4dd2c307d8a53b
Asif259 <ashrafulasif259@gmail.com>
2026-04-13 15:30:00 +0600

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Button variant="destructive">Button</Button>
     </div>
   );
 }
